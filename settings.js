5678184c2e55e1b84733dfb2fc876687f21b00f6
VINIX <148594993+VINIX100@users.noreply.github.com>
2025-05-22 15:38:00 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "malvin~9i0TEIhS#tSUYShhE4dRgW2GOSlaFCrFn7yL9RBAlaQdE2tvtsC0",
 // add your Session Id make sure it starts with malvin~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
